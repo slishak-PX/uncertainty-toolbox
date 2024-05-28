4c514cd0d6a2da7cb161c94ac9b8a067599066cc
Sam Lishak <sam.lishak@physicsx.ai>
2024-05-28 15:26:57 +0100

    Split Plotly stuff into separate module

--- uncertainty_toolbox/viz.py
+++ uncertainty_toolbox/viz.py
@@ -19,63 +19,7 @@ from uncertainty_toolbox.metrics_calibration import (
     get_proportion_lists_vectorized,
     adversarial_group_calibration,
 )
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import plotly.express as px
 
-
-def plotly_intervals(
-    y_pred,
-    y_std,
-    y_true,
-    n_subset=None,
-    ylims=None,
-    num_stds_confidence_bound=2,
-    show=True,
-):
-    """
-    Plot predicted values (y_pred) and intervals (y_std) vs observed
-    values (y_true).
-    """
-
-    if n_subset is not None:
-        [y_pred, y_std, y_true] = filter_subset([y_pred, y_std, y_true], n_subset)
-    intervals = num_stds_confidence_bound * y_std
-
-    # Plot
-    # fig = make_subplots()
-    fig = go.Figure(data=go.Scatter(
-        x=y_true,
-        y=y_pred,
-        mode='markers',
-        line=dict(color="#ff7f0e", width=2),
-        name="95% Interval",
-        error_y=dict(
-            color='rgba(255, 153, 0, 0.5)',
-            type='data',
-            array=intervals,
-            visible=True)
-    ))
-    fig.add_trace(go.Scatter(x=y_true, y=y_pred,  
-            mode='markers', 
-            name="Predicted", 
-            line=dict(color="#ff7f0e", width=2)))
-    fig.add_trace(go.Scatter(x=ylims, y=ylims, 
-                name="Observations",  
-                mode='lines', 
-                line=dict(color="#1f77b4", width=2)))
-
-    fig.update_xaxes(title_text="Observed Values")
-    fig.update_yaxes(title_text="<b>Predicted Values and Intervals</b>")
-    fig.update_layout(template="none", autosize=True, height=400, width=500)
-    fig.layout.font.family = 'Arial'
-    fig.update_layout(title="Prediction Intervals")
-
-    if ylims:
-        fig.update_layout(yaxis_range=[ylims[0], ylims[1]])
-        
-    if show:
-        fig.show()
 
 def plot_intervals(
     y_pred,
@@ -201,72 +145,6 @@ def plot_intervals_ordered(
         plt.show()
 
 
-def plotly_intervals_ordered(
-    y_pred,
-    y_std,
-    y_true,
-    n_subset=None,
-    ylims=None,
-    num_stds_confidence_bound=2,
-    show=True,
-):
-    """
-    Plot predicted values (y_pred) and intervals (y_std) vs observed
-    values (y_true).
-    """
-    if n_subset is not None:
-        [y_pred, y_std, y_true] = filter_subset([y_pred, y_std, y_true], n_subset)
-
-    order = np.argsort(y_true.flatten())
-    y_pred, y_std, y_true = y_pred[order], y_std[order], y_true[order]
-    xs = np.arange(len(order))
-    intervals = num_stds_confidence_bound * y_std
-
-    # Plot
-    fig = go.Figure(data=go.Scatter(
-        x=xs,
-        y=y_pred,
-        mode='markers',
-        line=dict(color="#ff7f0e", width=2),
-        name="95% Interval",
-        error_y=dict(
-            color='rgba(255, 153, 0, 0.5)',
-            type='data',
-            array=intervals,
-            visible=True)
-    ))
-    fig.add_trace(go.Scatter(x=xs, y=y_pred, 
-                mode='markers', 
-                name="Predicted", 
-                line=dict(color="#ff7f0e", width=2)))
-    fig.add_trace(go.Scatter(x=xs, y=y_true, 
-                name="Observations",  
-                mode='lines', 
-                line=dict(color="#1f77b4", width=2)))
-
-    # Determine lims
-    if ylims is None:
-        intervals_lower_upper = [y_pred - intervals, y_pred + intervals]
-        lims_ext = [
-            int(np.floor(np.min(intervals_lower_upper[0]))),
-            int(np.ceil(np.max(intervals_lower_upper[1]))),
-        ]
-    else:
-        lims_ext = ylims
-
-    fig.update_xaxes(title_text="Index (Ordered by Observed Value)")
-    fig.update_yaxes(title_text="<b>Predicted Values and Intervals</b>")
-    fig.update_layout(template="none", autosize=True, height=400, width=500)
-    fig.layout.font.family = 'Arial'
-    fig.update_layout(title="Ordered Prediction Intervals")
-
-    if ylims:
-        fig.update_layout(yaxis_range=[lims_ext[0], lims_ext[1]])
-        
-    if show:
-        fig.show()
-
-
 def plot_xy(
     y_pred,
     y_std,
@@ -316,58 +194,7 @@ def plot_xy(
         plt.show()
 
 
-def plotly_xy(
-    y_pred,
-    y_std,
-    y_true,
-    x,
-    n_subset=None,
-    ylims=None,
-    xlims=None,
-    num_stds_confidence_bound=2,
-    show=True,
-):
-    """Plot 1D input (x) and predicted/true (y_pred/y_true) values."""
-    if n_subset is not None:
-        [y_pred, y_std, y_true, x] = filter_subset([y_pred, y_std, y_true, x], n_subset)
-
-    intervals = num_stds_confidence_bound * y_std 
-
-    fig = make_subplots()
-    fig.add_trace(go.Scatter(x=x, y=y_pred,  
-                mode='markers', 
-                name="Predicted", 
-                line=dict(color="#ff7f0e", width=2)))
-    fig.add_trace(go.Scatter(x=x, y=y_true,  
-                mode='markers',  
-                name="True", 
-                line=dict(color="#1f77b4", width=2)))
-
-    fig.add_trace(go.Scatter(x=x, y=y_pred - intervals,
-        mode='lines', fill='none',
-        line=dict(color='rgba(0,0,0,0)'), 
-        hoverinfo="skip", showlegend=False))
-    fig.add_trace(go.Scatter(x=x, y=y_pred + intervals, 
-        mode='lines', fill='tonexty',
-        fillcolor='rgba(255, 153, 0, 0.2)', line=dict(color='rgba(0,0,0,0.0)'),
-        hoverinfo="skip", showlegend=True, name="95% Interval"))
- 
-    fig.update_xaxes(title_text="x")
-    fig.update_yaxes(title_text="<b>y</b>")
-    fig.update_layout(template="none", autosize=True, height=400, width=500)
-    fig.layout.font.family = 'Arial'
-    fig.update_layout(title="Confidence Band")
-
-    if ylims:
-        fig.update_layout(yaxis_range=[ylims[0],ylims[1]])
-        
-    if show:
-        fig.show()
-
-
-def plot_parity(
-    y_pred, y_true, n_subset=None, lims=None, axlabels=None, hexbins=False, show=False
-):
+def plot_parity(y_pred, y_true, n_subset=None, lims=None, axlabels=None, hexbins=False, show=False):
     """
     Make parity plot using predicted values (y_pred) and
     observed values (y_true).
@@ -485,9 +312,7 @@ def plot_calibration(
                 y_pred, y_std, y_true
             )
         else:
-            (exp_proportions, obs_proportions) = get_proportion_lists(
-                y_pred, y_std, y_true
-            )
+            (exp_proportions, obs_proportions) = get_proportion_lists(y_pred, y_std, y_true)
     else:
         # If expected and observed proportions are give
         exp_proportions = np.array(exp_props).flatten()
@@ -547,198 +372,6 @@ def plot_calibration(
         plt.show()
 
 
-def plotly_calibration(
-    y_pred,
-    y_std,
-    y_true,
-    n_subset=None,
-    curve_label=None,
-    show=True,
-    vectorized=True,
-    exp_props=None,
-    obs_props=None,
-):
-    """
-    Make calibration plot using predicted mean values (y_pred), predicted std
-    values (y_std), and observed values (y_true).
-    """
-    if n_subset is not None:
-        [y_pred, y_std, y_true] = filter_subset([y_pred, y_std, y_true], n_subset)
-
-    if (exp_props is None) or (obs_props is None):
-        # Compute exp_proportions and obs_proportions
-        if vectorized:
-            (exp_proportions, obs_proportions) = get_proportion_lists_vectorized(
-                y_pred, y_std, y_true
-            )
-        else:
-            (exp_proportions, obs_proportions) = get_proportion_lists(
-                y_pred, y_std, y_true
-            )
-    else:
-        # If expected and observed proportions are give
-        exp_proportions = np.array(exp_props).flatten()
-        obs_proportions = np.array(obs_props).flatten()
-        if exp_proportions.shape != obs_proportions.shape:
-            raise RuntimeError("exp_props and obs_props shape mismatch")
-
-    # Set figure defaults
-    fontsize = 12
-
-    # Set label
-    if curve_label is None:
-        curve_label = "Predictor"
-    # Plot
-    fig = make_subplots()
-    fig.add_trace(go.Scatter(x=[0, 1], y=[0, 1],  
-            mode='lines', 
-            name="Predicted", 
-            line=dict(color="#ff7f0e", width=2)))
-    fig.add_trace(go.Scatter(x=exp_proportions, y=obs_proportions,
-             mode='lines',  
-             name="True", line=dict(color="#1f77b4", width=2)))
-    fig.add_trace(go.Scatter(x=[0, 1], y=[0, 1], mode='lines', fill='none',
-            line=dict(color='rgba(0,0,0,0)'), 
-            hoverinfo="skip", showlegend=False))
-    fig.add_trace(go.Scatter(x=exp_proportions, y=obs_proportions, 
-            mode='lines', fill='tonexty',
-            fillcolor='rgba(255, 153, 0, 0.2)', 
-            line=dict(color='rgba(0,0,0,0.0)'), 
-            hoverinfo="skip", showlegend=True, name="95% Interval"))
- 
-    fig.update_xaxes(title_text="Predicted proportion in interval")
-    fig.update_yaxes(title_text="<b>Observed proportion in interval</b>")
-    fig.update_layout(template="none", autosize=True, height=400, width=500)
-    fig.layout.font.family = 'Arial'
-    fig.update_layout(title="Average Calibration")
-    
-    buff = 0.01
-    fig.update_layout(yaxis_range=[0 - buff, 1 + buff])
-   
-    # Compute miscalibration area
-    polygon_points = []
-    for point in zip(exp_proportions, obs_proportions):
-        polygon_points.append(point)
-    for point in zip(reversed(exp_proportions), reversed(exp_proportions)):
-        polygon_points.append(point)
-    polygon_points.append((exp_proportions[0], obs_proportions[0]))
-    polygon = Polygon(polygon_points)
-    x, y = polygon.exterior.xy  # original data
-    ls = LineString(np.c_[x, y])  # closed, non-simple
-    lr = LineString(ls.coords[:] + ls.coords[0:1])
-    mls = unary_union(lr)
-    polygon_area_list = [poly.area for poly in polygonize(mls)]
-    miscalibration_area = np.asarray(polygon_area_list).sum()
-
-    fig.add_annotation(x=1, y=0.1,
-                text="Miscalibration area = %.2f" % miscalibration_area,
-                showarrow=False,
-                arrowhead=1)
-    if show:
-        fig.show()
-
-
-def plotly_calibration_alt(
-    y_pred,
-    y_std,
-    y_true,
-    n_subset=None,
-    curve_label=None,
-    show=True,
-    vectorized=True,
-    exp_props=None,
-    obs_props=None,
-):
-    """
-    Make calibration plot using predicted mean values (y_pred), predicted std
-    values (y_std), and observed values (y_true).
-    """
-    if n_subset is not None:
-        [y_pred, y_std, y_true] = filter_subset([y_pred, y_std, y_true], n_subset)
-
-    if (exp_props is None) or (obs_props is None):
-        # Compute exp_proportions and obs_proportions
-        if vectorized:
-            (exp_proportions, obs_proportions) = get_proportion_lists_vectorized(
-                y_pred, y_std, y_true
-            )
-        else:
-            (exp_proportions, obs_proportions) = get_proportion_lists(
-                y_pred, y_std, y_true
-            )
-    else:
-        # If expected and observed proportions are give
-        exp_proportions = np.array(exp_props).flatten()
-        obs_proportions = np.array(obs_props).flatten()
-        if exp_proportions.shape != obs_proportions.shape:
-            raise RuntimeError("exp_props and obs_props shape mismatch")
-
-    # Set figure defaults
-    fontsize = 12
-
-    # Set label
-    if curve_label is None:
-        curve_label = "Predictor"
-    # Plot
-    fig = make_subplots()
-    fig.add_trace(go.Scatter(x=[0, 1], y=[0, 1],
-            mode='lines',
-            name="Predicted",
-            line=dict(color="#ff7f0e", width=2)))
-    fig.add_trace(go.Scatter(x=exp_proportions, y=obs_proportions,
-             mode='lines',
-             name="True", line=dict(color="#1f77b4", width=2)))
-    fig.add_trace(go.Scatter(x=[0, 1], y=[0, 1], mode='lines', fill='none',
-            line=dict(color='rgba(0,0,0,0)'),
-            hoverinfo="skip", showlegend=False))
-    fig.add_trace(go.Scatter(x=exp_proportions, y=obs_proportions,
-            mode='lines', fill='tonexty',
-            fillcolor='rgba(255, 153, 0, 0.2)',
-            line=dict(color='rgba(0,0,0,0.0)'),
-            hoverinfo="skip", showlegend=True, name="95% Interval"))
-
-    fig.update_layout(template="none", autosize=True, height=500, width=530)
-    fig.layout.font.family = 'Georgia'
-    fig.update_layout(title="Average Calibration")
-
-    buff = 0.01
-    fig.update_layout(xaxis_range=[0 - buff, 1 + buff])
-    fig.update_layout(yaxis_range=[0 - buff, 1 + buff])
-
-    fig.update_xaxes(
-        title_text="Predicted proportion in interval",
-        tickvals=[0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
-    )
-    fig.update_yaxes(
-        title_text="Observed proportion in interval",
-        scaleanchor = "x",
-        scaleratio = 1,
-        tickvals=[0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
-    )
-
-    # Compute miscalibration area
-    polygon_points = []
-    for point in zip(exp_proportions, obs_proportions):
-        polygon_points.append(point)
-    for point in zip(reversed(exp_proportions), reversed(exp_proportions)):
-        polygon_points.append(point)
-    polygon_points.append((exp_proportions[0], obs_proportions[0]))
-    polygon = Polygon(polygon_points)
-    x, y = polygon.exterior.xy  # original data
-    ls = LineString(np.c_[x, y])  # closed, non-simple
-    lr = LineString(ls.coords[:] + ls.coords[0:1])
-    mls = unary_union(lr)
-    polygon_area_list = [poly.area for poly in polygonize(mls)]
-    miscalibration_area = np.asarray(polygon_area_list).sum()
-
-    fig.add_annotation(x=0.77, y=0.1,
-                text="Miscalibration area = %.2f" % miscalibration_area,
-                showarrow=False,
-                arrowhead=1)
-    if show:
-        fig.show()
-
-
 def plot_adversarial_group_calibration(
     y_pred,
     y_std,
@@ -772,12 +405,8 @@ def plot_adversarial_group_calibration(
         group_size = np.array(group_size).flatten()
         score_mean = np.array(score_mean).flatten()
         score_stderr = np.array(score_stderr).flatten()
-        if (group_size.shape != score_mean.shape) or (
-            group_size.shape != score_stderr.shape
-        ):
-            raise RuntimeError(
-                "Input arrays for adversarial group calibration shape mismatch"
-            )
+        if (group_size.shape != score_mean.shape) or (group_size.shape != score_stderr.shape):
+            raise RuntimeError("Input arrays for adversarial group calibration shape mismatch")
 
     # Set label
     if curve_label is None:
@@ -827,7 +456,7 @@ def plot_sharpness(y_std, n_subset=None):
     ax_sharp.set_yticks([])
 
     # Calculate and report sharpness
-    sharpness = np.sqrt(np.mean(y_std ** 2))
+    sharpness = np.sqrt(np.mean(y_std**2))
     _ = ax_sharp.axvline(x=sharpness, label="sharpness")
     if sharpness < (xlim[0] + xlim[1]) / 2:
         text = "\n  Sharpness = %.2f" % sharpness
@@ -882,11 +511,7 @@ if __name__ == "__main__":
     import metrics_calibration
 
     y_pred, y_std, y_true, x_true = data.synthetic_sine_heteroscedastic(100)
-    print(
-        metrics_calibration.adversarial_group_calibration(
-            y_pred, y_std, y_true, "root_mean_sq"
-        )
-    )
+    print(metrics_calibration.adversarial_group_calibration(y_pred, y_std, y_true, "root_mean_sq"))
     plot_calibration(y_pred, y_std, y_true, show=True)
     plot_adversarial_group_calibration(y_pred, y_std, y_true, show=True)
     plot_sharpness(y_std)
